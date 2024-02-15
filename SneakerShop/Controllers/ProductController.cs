400808e30811b00acd5a82b8435a749a0a3c1fa5
ItayDosik <itaydo@ac.sce.ac.il>
2024-02-15 14:16:53 +0200

    new site whos this

---
+++ SneakerShop/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SneakerShop.Models;
+
+namespace SneakerShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Enter()
+        {
+            return View();
+        }
+
+        public ActionResult Submit(Product product)
+        {
+            return View("Product", product);
+        }
+
+    }
+
+
+}
---
+++ SneakerShop/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SneakerShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string Price { get; set; }
+        public string ProductPictureURL { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Category { get; set; }
+    }
+}
