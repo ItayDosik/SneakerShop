2a5d54793311960436c37493773aa122353bd9a6
ItayDosik <itaydo@ac.sce.ac.il>
2024-02-24 17:51:35 +0200

    viewallproducts

---
+++ SneakerShop/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SneakerShop.Models
+{
+    public class Products
+    {
+        public Products() 
+        { 
+            allProducts = new List<Product>(); 
+        }
+        public List<Product> allProducts { get; set; }
+    }
+}
