6d6a39db51dee9ba22016707a8d4b6fc6663b7de
ItayDosik <itaydo@ac.sce.ac.il>
2024-02-29 19:25:05 +0200

    wip

--- SneakerShop/Controllers/ProductController.cs
+++ SneakerShop/Controllers/ProductController.cs
@@ -33,8 +33,30 @@ namespace SneakerShop.Controllers
         {
             _db.Products.Add(product);
             _db.SaveChanges();
-            return View("NewProduct");
+            return View("ViewAllProducts");
 
+        }
+
+        public ActionResult Edit(int id) 
+        {
+            Product product = _db.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product _product)
+        {
+            Product product = _db.Products.Find(_product.ProductId);
+            product.ProductName = _product.ProductName;
+            product.ProductDescription = _product.ProductDescription;
+            product.Price = _product.Price;
+            product.ProductPictureURL1 = _product.ProductPictureURL1;
+            product.ProductPictureURL2 = _product.ProductPictureURL2;
+            product.ProductPictureURL3 = _product.ProductPictureURL3;
+            product.Category = _product.Category;
+            product.Size = _product.Size;
+            _db.SaveChanges();
+            return View("ViewAllProducts");
         }
 
         public ActionResult ViewAllProducts()
