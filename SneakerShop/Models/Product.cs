4514b4eef66d4b312e34bdb37cc4217601b07416
ItayDosik <itaydo@ac.sce.ac.il>
2024-03-03 10:53:49 +0200

    bug fix

--- SneakerShop/Models/Product.cs
+++ SneakerShop/Models/Product.cs
@@ -6,14 +6,24 @@ namespace SneakerShop.Models
     {
         [Key]
         public int ProductId { get; set; }
+        [Required]
         public string ProductName { get; set; }
+        [Required]
         public string ProductDescription { get; set; }
+        [Required]
+        [Range(0, 9999999999999999.99)]
         public decimal Price { get; set; }
+        [Required]
         public string ProductPictureURL1 { get; set; }
+        [Required]
         public string ProductPictureURL2 { get; set; }
+        [Required]
         public string ProductPictureURL3 { get; set; }
+        [Required]
         public int Qnt { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public string Size { get; set; }
 
     }
