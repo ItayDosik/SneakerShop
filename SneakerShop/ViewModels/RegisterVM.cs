bd1d2d2a5f5eb7f2c53c07636fb0f768e3e054f6
AdivEliyahu <Adiveliyahu3@gmail.com>
2024-03-27 16:11:13 +0200

    register fixes

--- SneakerShop/ViewModels/RegisterVM.cs
+++ SneakerShop/ViewModels/RegisterVM.cs
@@ -5,28 +5,42 @@ namespace SneakerShop.ViewModels
     public class RegisterVM
     {
         
-        [Required]
+        [Required(ErrorMessage = "Full name required.")]
+        [RegularExpression("^[A-Za-z][a-z]+\\s[A-Za-z][a-z]+$", ErrorMessage = "Invalid full name.")]
         public string? Name { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "User Name is required")]
         public string? UserName { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "Phone number is required")]
         [RegularExpression("^0(5[0-9])\\d{7}$", ErrorMessage = "Invalid Phone number.")]
         public string? PhoneNumber { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "Email is required.")]
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
         public string? Email { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "Address is required.")]
         
         public string? Address { get; set; }
-        [Required]
-        [DataType(DataType.Password)]
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "The field must be at least 6 characters long.")]
         public string? Password { get; set; }
+
+
 
         [Compare("Password", ErrorMessage = "Passwords are not the same.")]
         [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
         public string? ConfirmPassword { get; set; }
+
 
         public string? Role {  get; set; } 
     }
